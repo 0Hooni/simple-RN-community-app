f4fd7b6df82269ceecc27e739f5596b4e0eb889a
0Hooni <thddudgns972@gmail.com>
2025-07-24 14:34:38 +0900

    feat/#3: 버튼 컴포넌트 생성
    - 필요한 타이포 시스템 추가
    - 임시 컴포넌트 형태로 구현
    - 컴포넌트 일괄 export를 위한 index 추가

---
+++ sample-community/src/components/Button.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components/native";
+import { typography } from "../styles";
+
+const Text = styled.Text`
+  font-size: ${typography.title3.fontSize}px;
+  font-weight: ${typography.title3.fontWeight};
+  line-height: ${typography.title3.lineHeight}px;
+  color: white;
+`
+
+const TouchableOpacity = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  width: 240px;
+  height: 48px;
+  border-radius: 16px;
+`
+
+export const Button = ({ text, backgroundColor }: { text: string, backgroundColor: string }) => {
+  return <TouchableOpacity style={{ backgroundColor }}>
+    <Text>{text}</Text>
+  </TouchableOpacity>
+}
--- sample-community/src/styles/typography.ts
+++ sample-community/src/styles/typography.ts
@@ -2,6 +2,16 @@ import styled from 'styled-components/native';
 
 // 타이포그래피 토큰 정의
 export const typography = {
+  title1: {
+    fontSize: 28,
+    fontWeight: 'normal' as const,
+    lineHeight: 34,
+  },
+  title3: {
+    fontSize: 20,
+    fontWeight: 'normal' as const,
+    lineHeight: 25,
+  },
   body: {
     fontSize: 17,
     fontWeight: 'normal' as const,
