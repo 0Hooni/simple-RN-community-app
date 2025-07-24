94fb216bf13ed4f7b7cbbc2044730d8bedcf881b
0Hooni <thddudgns972@gmail.com>
2025-07-24 14:34:48 +0900

    feat/#3: 로그인 화면 구현

--- sample-community/app/index.tsx
+++ sample-community/app/index.tsx
@@ -1,15 +1,48 @@
-import { Text, View } from "react-native";
+import styled from "styled-components/native";
+import { TouchableWithoutFeedback, Keyboard } from "react-native";
+import { TextField, Button } from "../src/components";
+import { typography } from "../src/styles";
+
+const Title = styled.Text`
+  font-size: ${typography.title1.fontSize}px;
+  font-weight: ${typography.title1.fontWeight};
+  line-height: ${typography.title1.lineHeight}px;
+`
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  gap: 140px;
+`
+
+const TextFieldContainer = styled.View`
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px 36px;
+  gap: 20px;
+`
+
+const ButtonContainer = styled.View`
+  flex-direction: column;
+  gap: 20px;
+`
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <Container>
+      <Title>로그인</Title>
+      <TextFieldContainer>
+        <TextField title="이메일" placeholder="이메일을 입력해주세요." keyboardType="email-address" />
+        <TextField title="비밀번호" placeholder="비밀번호를 입력해주세요." keyboardType="visible-password" />
+      </TextFieldContainer>
+      <ButtonContainer>
+        <Button text="로그인" backgroundColor="blue" />
+        <Button text="회원가입" backgroundColor="gray" />
+        </ButtonContainer>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
