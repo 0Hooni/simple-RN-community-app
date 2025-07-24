b92728106204f6a4996761f90ff7cb3f11b738c6
0Hooni <thddudgns972@gmail.com>
2025-07-24 14:00:44 +0900

    feat/#3: 타이포그래피 정의
    - 우선 사용하는것만 추가하고, 추후에 필요할때마다 확장 예정

---
+++ sample-community/src/styles/typography.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+// 타이포그래피 토큰 정의
+export const typography = {
+  body: {
+    fontSize: 17,
+    fontWeight: 'normal' as const,
+    lineHeight: 22,
+  },
+} as const;
