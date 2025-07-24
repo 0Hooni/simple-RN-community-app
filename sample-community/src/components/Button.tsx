911068885e6fdee5d2ff3ec75df8ccb6f5a27a9c
0Hooni <thddudgns972@gmail.com>
2025-07-24 14:38:34 +0900

    feat/#3: Button onPress 추가

--- sample-community/src/components/Button.tsx
+++ sample-community/src/components/Button.tsx
@@ -1,12 +1,12 @@
-import styled from "styled-components/native";
-import { typography } from "../styles";
+import styled from 'styled-components/native';
+import { typography } from '../styles';
 
 const Text = styled.Text`
   font-size: ${typography.title3.fontSize}px;
   font-weight: ${typography.title3.fontWeight};
   line-height: ${typography.title3.lineHeight}px;
   color: white;
-`
+`;
 
 const TouchableOpacity = styled.TouchableOpacity`
   justify-content: center;
@@ -14,10 +14,20 @@ const TouchableOpacity = styled.TouchableOpacity`
   width: 240px;
   height: 48px;
   border-radius: 16px;
-`
+`;
 
-export const Button = ({ text, backgroundColor }: { text: string, backgroundColor: string }) => {
-  return <TouchableOpacity style={{ backgroundColor }}>
-    <Text>{text}</Text>
-  </TouchableOpacity>
-}
+export const Button = ({
+  text,
+  backgroundColor,
+  onPress,
+}: {
+  text: string;
+  backgroundColor: string;
+  onPress: () => void;
+}) => {
+  return (
+    <TouchableOpacity style={{ backgroundColor }} onPress={onPress}>
+      <Text>{text}</Text>
+    </TouchableOpacity>
+  );
+};
