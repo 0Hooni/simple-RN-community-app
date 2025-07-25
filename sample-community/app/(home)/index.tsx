d4866bcfcc0b7d1aa962cb0fbe298d7f78200f7b
0Hooni <thddudgns972@gmail.com>
2025-07-25 10:25:51 +0900

    feat/#5: 포스팅 프리뷰 컴포넌트 생성

--- sample-community/app/(home)/index.tsx
+++ sample-community/app/(home)/index.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native';
+import { PostPreView } from '../../src/components/PostPreView';
+import styled from 'styled-components/native';
+
+const Container = styled.ScrollView`
+  flex: 1;
+  background-color: white;
+`;
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <Container>
+      <PostPreView
+        title="제목"
+        writer="작성자"
+        timeStamp="2025-01-01"
+        onPress={() => {}}
+      />
+      <PostPreView
+        title="제목"
+        writer="작성자"
+        timeStamp="2025-01-01"
+        onPress={() => {}}
+      />
+    </Container>
   );
 }
